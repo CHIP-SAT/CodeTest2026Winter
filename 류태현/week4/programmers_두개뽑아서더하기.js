03adf52679a6c1c6efd5becf2eb4ae0acd7ce3fd
Ryu Tae Hyun <ryu200112@gmail.com>
2026-02-08 16:26:04 +0900

    프로그래머스 레벨1: 두뽑더, 숫문영

---
+++ 류태현/week4/programmers_두개뽑아서더하기.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+  var arr = [];
+
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      arr.push(numbers[i] + numbers[j]);
+    }
+  }
+
+  var answer = [...new Set(arr)].sort((a, b) => a - b);
+
+  return answer;
+}
